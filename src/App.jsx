3d188d977f64de1deb706f33cc3362b28bebae11
thamyris <thamyriscabral@hotmail.com>
2023-02-20 15:59:36 -0300

    exemplos termidados

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import reactLogo from './assets/react.svg';
 // import PropTypes from 'prop-types';
 import './App.css';
@@ -11,6 +11,19 @@ const Button = React.memo(({ incrementButton }) => {
 // Button.propTypes = {
 //   incrementButton: PropTypes.func,
 // };
+
+const GlobalContext = React.createContext();
+
+const globalState = {
+  mode: 'dark',
+  count: 0,
+};
+
+const H1 = () => {
+  const theContext = useContext(GlobalContext);
+  console.log(theContext.context.count);
+  return <p onClick={() => theContext.setContext(() => {})}>{theContext.context.count}</p>;
+};
 
 function App() {
   const [reverse, setReverse] = useState(false);
@@ -33,45 +46,55 @@ function App() {
     setValue(e.target.textContent);
   };
 
+  const handleDarkLigth = () => {
+    setContext(context === 'dark' ? 'ligth' : 'dark');
+  };
+
   useEffect(() => {
     contador.current++;
 
     input.current.focus();
   }, [value]);
 
+  const [context, setContext] = useState(globalState);
+
   return (
-    <div className='App'>
-      <div>
-        <a
-          href='https://vitejs.dev'
-          target='_blank'
-        ></a>
-        <a
-          href='https://reactjs.org'
-          target='_blank'
-        >
-          <img
-            src={reactLogo}
-            className={`logo react ${handleR}`}
-            alt='React logo'
-          />
-        </a>
+    <GlobalContext.Provider value={{ context, setContext }}>
+      <div className={`App ${context}`}>
+        <button onClick={handleDarkLigth}>dark/ligth</button>
+        <H1 />
+        <div>
+          <a
+            href=''
+            target='_blank'
+          ></a>
+          <a
+            href=''
+            target='_blank'
+          >
+            <img
+              src={reactLogo}
+              className={`logo react ${handleR}`}
+              alt='React logo'
+            />
+          </a>
+        </div>
+        <div className='card'>
+          <button onClick={handleReverse}>change</button>
+          <h1>contador de letras: {contador.current}</h1>
+          <h1 onClick={handleName}>Count: {count}</h1>
+          <Button incrementButton={incrementButton} />
+        </div>
+        <p>{value}</p>
+        <input
+          ref={input}
+          type='text'
+          value={value}
+          placeholder={'escreva'}
+          onChange={(e) => setValue(e.target.value)}
+        />
       </div>
-      <div className='card'>
-        <button onClick={handleReverse}>change</button>
-        <h1>contador de letras: {contador.current}</h1>
-        <h1 onClick={handleName}>Count: {count}</h1>
-        <Button incrementButton={incrementButton} />
-      </div>
-      <p>{value}</p>
-      <input
-        ref={input}
-        type='text'
-        value={value}
-        placeholder={'escreva'}
-        onChange={(e) => setValue(e.target.value)}
-      />
-    </div>
+    </GlobalContext.Provider>
   );
 }
 
